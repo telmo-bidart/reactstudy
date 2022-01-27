a92fd4f43a9e56b0f1b39e6cccb021ebcd5161db
telmo bidart <telmobidart@gmail.com>
2022-01-27 20:03:02 -0300

    pessoa added to app js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Pessoa from './components/Pessoa';
 import SayMyName  from './components/SayMyName';
+
 
 function App() {
 
   return (
     <div className="App">
-      <SayMyName nome="telmo"/> 
+      <SayMyName nome="telmo"/>
+      <Pessoa nome="Telmo" idade='22' profissao='desempregado' foto="https://via.placeholder.com/150" />
+
     </div>
   );
 }
--- src/components/Pessoa.js
+++ src/components/Pessoa.js
@@ -1,5 +1,9 @@
-function Pessoa () {
+function Pessoa (props) {
     return <div>
+        <img src={props.foto} alt={props.nome} />
+        <h2>Nome: {props.nome}</h2>
+        <p>Idade: {props.idade}</p>
+        <p>Profissão: {props.profissao}</p>
 
     </div>
 }
