cc67ca9e29f5ab8d0e83006117be69d637bd49bc
telmo bidart <telmobidart@gmail.com>
2022-01-27 20:08:41 -0300

    props desestrctured

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import HelloWorld from './components/helloworld';
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <HelloWorld />
   </React.StrictMode>,
   document.getElementById('root')
 );
