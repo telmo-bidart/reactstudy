a3acba3706d61a9873a22292c061d896910c23e5
telmo bidart <telmobidart@gmail.com>
2022-01-31 22:48:21 -0300

    altura

--- src/components/Pessoa.js
+++ src/components/Pessoa.js
@@ -1,8 +1,9 @@
-function Pessoa ({nome, idade, profissao, foto}) {
+function Pessoa ({nome, idade, profissao, altura, foto}) {
     return <div>
         <img src={foto} alt={nome} />
         <h2>Nome: {nome}</h2>
         <p>Idade: {idade}</p>
+        <p>ALtura: {altura}</p>
         <p>Profissão: {profissao}</p>
 
     </div>
