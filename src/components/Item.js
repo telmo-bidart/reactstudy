f634af154918c4d60088c6e09119c9744215622e
telmo bidart <telmobidart@gmail.com>
2022-02-03 08:18:54 -0300

    aula de proptypes

---
+++ src/components/Item.js
@@ -0,0 +1,9 @@
+function Item({marca, ano_lançamento}) {
+    return (
+        <>
+            <li>{marca} - {ano_lançamento}   </li>
+        </>
+    )
+}
+
+export default Item
--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,28 @@
+import PropTypes from "prop-types";
+
+import Item from "./Item"
+
 function List () {
     return (
         <>
         <h1>
-            <ul>essa é uma lista</ul>
-            <ul>subitem
-                
+            <ul>
+                <Item marca="ferrari" ano_lançamento={2021} />
+                <Item marca="lamborghini" ano_lançamento={2021} />
             </ul>
         </h1>
         </>
     )
+}
+
+Item.propTypes = {
+    marca: PropTypes.string,
+    ano_lançamento: PropTypes.number
+}
+
+Item.defaultProps = {
+    marca: "faltou marca",
+    ano_lançamento: 0
 }
 
 export default List
