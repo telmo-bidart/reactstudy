c0c7208185137566f9c7f5fdc3e46ffcd85ebefb
telmo bidart <telmobidart@gmail.com>
2022-02-03 08:47:48 -0300

    aula de usestate

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,23 +1,45 @@
+import { useState } from 'react'
+
 function Form() {
+  function cadastraUsuario(e) {
+    e.preventDefault();
+    console.log(name)
+    console.log(password)
+    console.log("cadastrou");
+  }
 
-    function cadastraUsuario(e) {
-        e.preventDefault()
-        console.log("cadastrou")
-    }
+  const [name, setName] = useState()
+  const [password, setPassword] = useState()
 
-    return (
+  return (
+    <div>
+      <h1>Meu cadastro:</h1>
+      <form onSubmit={cadastraUsuario}>
         <div>
-            <h1>Meu cadastro:</h1>
-            <form onSubmit={cadastraUsuario}>
-            <div>
-                <input type="text" placeholder="Digite seu nome" />
-            </div>
-            <div>
-            <input type="submit" value="Cadastrar" />
-             </div>
-            </form>
+          <label htmlFor="name">Nome:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Digite seu nome"
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
-    
-    )
+        <div>
+          <label htmlFor="password">Senha:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Digite sua senha"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <input type="submit" value="Cadastrar" />
+        </div>
+      </form>
+    </div>
+  );
 }
-export default Form
+export default Form;
